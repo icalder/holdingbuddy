b69f1701b8dbbe122d451815b4f54474611a0e2d
Iain Calder <iain.calder@gmail.com>
2020-03-15 22:04:31 +0000

    Rewrite include paths when publishing to github pages

--- rollup.config-publish.js
+++ rollup.config-publish.js
@@ -1,13 +1,20 @@
 import copy from 'rollup-plugin-copy';
 
-const publishDest = '../holdingbuddy-public';
+const publishDest = '../icalder.github.io/holdingbuddy';
 
 import config from './rollup.config.js';
 config.plugins.push(
   copy({
     targets: [
       { src: 'dist/bundle.js', dest: `${publishDest}/dist` },
-      { src: 'index.html', dest: publishDest },
+      {
+        src: 'index.html',
+        dest: publishDest,
+        transform: contents =>
+          contents.toString()
+            .replace(/\/static\//g, '/holdingbuddy/static/')
+            .replace('/dist/', '/holdingbuddy/dist/')
+      },
       { src: 'static/**/*', dest: `${publishDest}/static`}
     ]
   })

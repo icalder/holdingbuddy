582bf59e0602a7c505690ef30ae4777be2622e0d
Iain Calder <iain.calder@gmail.com>
2020-03-15 22:27:07 +0000

    Fix publish path rewrites and icon folders

--- rollup.config-publish.js
+++ rollup.config-publish.js
@@ -6,7 +6,11 @@ import config from './rollup.config.js';
 config.plugins.push(
   copy({
     targets: [
-      { src: 'dist/bundle.js', dest: `${publishDest}/dist` },
+      {
+        src: 'dist/bundle.js', 
+        dest: `${publishDest}/dist`,
+        hook: 'writeBundle'
+      },
       {
         src: 'index.html',
         dest: publishDest,
@@ -15,7 +19,18 @@ config.plugins.push(
             .replace(/\/static\//g, '/holdingbuddy/static/')
             .replace('/dist/', '/holdingbuddy/dist/')
       },
-      { src: 'static/**/*', dest: `${publishDest}/static`}
+      {
+        src: 'static/*.css',
+        dest: `${publishDest}/static`,
+        transform: contents =>
+          contents.toString()
+            .replace(/\/static\//g, '/holdingbuddy/static/')
+      },
+      {
+        src: ['static/*', '!static/*.css'],
+        dest: `${publishDest}/static`,
+        flatten: false
+      }
     ]
   })
 );
